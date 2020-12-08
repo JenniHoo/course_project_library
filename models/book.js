a6d298e6a0a248a246a8a184fade7e439da7ba93
voittamatonkulho <kim.backman@edu.turkuamk.fi>
2020-12-08 18:53:33 +0200

    Added database functionality

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bookSchema = mongoose.Schema({
     name: String,
     author: String,
-    shelLocation:  String,
+    shelfLocation:  String,
     numberAvailable: Number
 })
 
