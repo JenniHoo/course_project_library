6ae3df9831ceb2af2cc3dc1bf392e9ca8763bacd
Anna <AL13NZ@users.noreply.github.com>
2020-12-09 16:58:58 +0200

    Made a route for booklist and created user model

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -10,6 +10,22 @@ const getBookParams = body => {
 }
 
 module.exports = {
+   index: (req, res, next) => {
+        Book.find({})
+            .then(books => {
+                res.locals.books = books;
+                next();
+            })
+            .catch(error => {
+                console.log(`Couldn't get books: ${error.message}`)
+                next(error);
+            })
+   },
+
+   indexView: (req, res) => {
+        res.render("booklist");
+   },
+
     create: (req, res, next) => {
         let bookParams = getBookParams(req.body);
         console.log(bookParams);
@@ -34,5 +50,18 @@ module.exports = {
         else {
             next();
         }
+    },
+
+    delete: (req, res, next) =>{
+        let bookID = req.params.id;
+        Book.findByIdAndDelete(bookID)
+        .then(() => {
+            res.locals.redirect = "/books";
+            next();
+        })
+        .catch(error => {
+            console.log(`Couldn't delete book: ${error.message}`);
+            next();
+        }) 
     }
 }
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String, 
+        required: true,
+        lowercase:true,
+        unique: true 
+    }
+})
+
+module.exports = mongoose.model("User", userSchema);
